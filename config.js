3b6fafb9e9a0a78156d6df4e0eb0d2167d530152
crash springfield <matthew.boman@gmail.com>
2016-11-14 14:53:14 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports.setConfigs = function() {
+  process.env.MONGO_CONNECT = "",
+  process.env.CLOUDINARY_URL = ""
+}
+
+// HMU if you want to use the database for pics of my cat. Or just use your own to compare your pet to others
